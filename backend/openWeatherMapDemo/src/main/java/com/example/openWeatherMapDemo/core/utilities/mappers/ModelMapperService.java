d670fddb63227d11694281c886bba8927f52a148
Ahmet Tarık Fırat <113821940+tarikfrt@users.noreply.github.com>
2024-05-26 01:29:54 +0300

    Initial start commit

--- backend/openWeatherMapDemo/src/main/java/com/example/openWeatherMapDemo/core/utilities/mappers/ModelMapperService.java
+++
@@ -1,8 +0,0 @@
-package com.example.openWeatherMapDemo.core.utilities.mappers;
-
-import org.modelmapper.ModelMapper;
-
-public interface ModelMapperService {
-	ModelMapper forResponse();
-	ModelMapper forRequest();
-}
